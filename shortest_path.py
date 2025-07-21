8dbb82098690f0ef0a3f35910b4d1c3ee48a1029
Amin-khattab <aminghost47@gmail.com>
2025-07-21 18:16:06 +0300

    Add files via upload

---
+++ shortest_path.py
@@ -0,0 +1,26 @@
+graph = {
+    "A" : {"B":2},
+    "B" : {"C":3},
+    "C" : {"D":1},
+    "D" : {}
+}
+
+
+def main(graph,start,end):
+
+    current = start
+    total_dist = 0
+    path = [start]
+
+    while current != end:
+        next_node = list(graph[current].keys())[0]
+        distance = graph[current][next_node]
+        total_dist += distance
+        path.append(next_node)
+        current = next_node
+
+    return path,total_dist
+
+path,dist = main(graph,"A","D")
+print(path)
+print(dist)
