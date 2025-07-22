7a7bf53f81cba0bc66454535e9e939d6e622d56a
Amin-khattab <aminghost47@gmail.com>
2025-07-22 16:22:28 +0300

    Add files via upload

---
+++ shortest_path(meduim).py
@@ -0,0 +1,46 @@
+graph = {
+    "A": {"B": 2, "C": 3},
+    "B": {"D": 1, "C": 3},
+    "C": {"D": 4},
+    "D": {}
+}
+
+def main(gragh,start,end):
+
+    path = [start]
+    current = start
+    total_distance = 0
+
+    while current != end:
+        neighbor = list(gragh[current].keys())
+
+        if len(neighbor) > 1 :
+
+            first_neighbor = neighbor[0]
+            second_neighbor = neighbor[1]
+
+            first_dist = gragh[current][first_neighbor]
+            second_dist = gragh[current][second_neighbor]
+
+            if first_dist > second_dist:
+                distance = second_dist
+                next_node = second_neighbor
+            else:
+                distance = first_dist
+                next_node = first_neighbor
+
+            total_distance = total_distance + distance
+            path.append(next_node)
+            current = next_node
+        else:
+            next_node = list(gragh[current].keys())[0]
+            distance = gragh[current][next_node]
+            total_distance = total_distance + distance
+            path.append(next_node)
+            current = next_node
+
+    return path,total_distance
+path,dist = main(graph,"A","D")
+
+print(path)
+print(dist)
