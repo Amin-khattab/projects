c571018f52f71acf0702b821bb3d99b455dae56f
Amin Khattab <aminghost47@gmail.com>
2026-01-01 19:48:46 +0300

    Refactor GPT-2 fine-tuning script and update parameters

--- GPT-2-FineTuned.py
+++ GPT-2-FineTuned.py
@@ -1,42 +1,63 @@
+from tensorflow.python.keras.utils.version_utils import training
+from transformers import GPT2Tokenizer,GPT2LMHeadModel,Trainer,TrainingArguments,DataCollatorForLanguageModeling
 from datasets import load_dataset
-from transformers import GPT2Tokenizer,GPT2LMHeadModel,TrainingArguments,Trainer,pipeline
-
-dataset = load_dataset("text",data_files={"train":"Data.txt"})# <===== you need to put your data here 
+import time
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
 
-def preprocess(example):
-    inputs =  tokenizer(example['text'], truncation=True, padding='max_length', max_length=128)
-    inputs["labels"] = inputs["input_ids"].copy()
-    return inputs
-
-tokenized_dataset = dataset["train"].map(preprocess,batched=True)
-
 model = GPT2LMHeadModel.from_pretrained("gpt2")
-model.resize_token_embeddings(len(tokenizer))
 
-training_args = TrainingArguments(
-    output_dir="./GPT-2",
-    per_device_train_batch_size=2,
-    num_train_epochs=5,
-    logging_steps=10,
-    save_steps=50,
-    save_total_limit=2,
-    fp16=False
+dataset = load_dataset("wikitext","wikitext-2-raw-v1",split="train")
+
+def preprocess(example):
+    return tokenizer(example["text"],padding="max_length",truncation=True,max_length=128)
+
+tokenized_dataset = dataset.map(preprocess,batch_size=True)
+
+datacollator = DataCollatorForLanguageModeling(mlm=False,tokenizer=tokenizer)
+
+training_arguments = TrainingArguments(
+    num_train_epochs=3,
+    save_steps=1000,
+    logging_steps=100,
+    learning_rate=5e-5,
+    per_device_train_batch_size=16,
+    output_dir="./MY_GPT"
 )
 
 trainer = Trainer(
-    model = model,
-    train_dataset=tokenized_dataset,
-    args =training_args,
-    tokenizer = tokenizer
+    args=training_arguments,
+    data_collator=datacollator,
+    model=model,
+    train_dataset=tokenized_dataset
 )
 
 trainer.train()
 
-model.save_pretrained("gpt2-finetuned")
-tokenizer.save_pretrained("gpt2-finetuned")
 
-generator = pipeline("text-generation",model="gpt2-finetuned",tokenizer="gpt2-finetuned")
-print(generator("my name is", max_length = 50,num_return_sequences = 1))
+#PIPELINE
+
+import torch
+from transformers import pipeline
+
+chatbot = pipeline("text-generation",model="./MY_GPT",tokenizer="./MY_GPT")
+
+print("input 'exit' or 'quit' to abbort")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() in ["exit","quit"]:
+        break
+
+    response = chatbot(
+        user_input,
+        max_length = len(user_input) + 40,
+        num_return_sequences = 1,
+        do_sample=True,
+        temperature=2.0,
+        pad_token_id=50256
+    )
+
+    print("Bot: ", response[0])
